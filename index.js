819187950591f086a115a2b563b4dc91684e2d30
femaledurk <chelseataylor.moore25@compscihigh.org>
2025-02-28 17:07:54 +0000

    done

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Step 0: Make sure you have Express installed (`npm install express`).
 
 // Step 1: Import the Express module
-
+const express = require ("express")
 // Step 2: Create an instance of an Express application
+const app = express ();
 
+
+app.get ("/", (req,res)=>{
+    res.send ("hello")
+})
 // Step 3: Start the server on port 3000
-
+app.listen (3000,()=>{
+    console.log("Server is running at http://localhost:3000")
+})
 // Step 4: Run this file, use the command: `node index.js`
